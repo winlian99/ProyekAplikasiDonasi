8f4355fdf1ebc40ba5e89be5fbe56379a976db8b
handy <c14170131@john.petra.ac.id>
2020-12-08 17:13:24 +0700

    UI Profile Selesai

--- app/src/main/java/com/example/proyekaplikasidonasi/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/proyekaplikasidonasi/ui/profile/ProfileFragment.kt
@@ -22,10 +22,10 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        profile_dompet_saya.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_account_balance_wallet_24, 0,0,0)
-        profile_galang_dana_saya.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_add_box_24, 0, 0, 0)
-        profile_bantuan.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_contact_support_24, 0,0,0)
-        profile_sign_out.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_assignment_return_24, 0, 0, 0)
+//        profile_dompet_saya.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_account_balance_wallet_24, 0,0,0)
+//        profile_galang_dana_saya.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_add_box_24, 0, 0, 0)
+//        profile_bantuan.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_contact_support_24, 0,0,0)
+//        profile_sign_out.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_assignment_return_24, 0, 0, 0)
 
         profile_dompet_saya.setOnClickListener {
             Toast.makeText(context,"Bisa ngeklik text",Toast.LENGTH_SHORT).show()
