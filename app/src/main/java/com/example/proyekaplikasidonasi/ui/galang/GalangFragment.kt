323675b088ceb5691cc4a50a8f6e70bcb1a93394
Jason <c14170129@john.petra.ac.id>
2020-12-18 16:04:11 +0700

    Attempt: Prettify Galang Fragment

--- app/src/main/java/com/example/proyekaplikasidonasi/ui/galang/GalangFragment.kt
+++ app/src/main/java/com/example/proyekaplikasidonasi/ui/galang/GalangFragment.kt
@@ -13,6 +13,9 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_galang.*
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.*
 
 class GalangFragment : Fragment() {
 
@@ -38,15 +41,24 @@ class GalangFragment : Fragment() {
         db.collection(dbCol).document(userId).get().addOnSuccessListener {
             var data = it?.data as MutableMap<String, String>
             judul_galang.setText(data.get("name").toString())
-            text_progress.setText("Rp. " + data.get("donated_cash").toString()
-                    + " terkumpul dari Rp. " + data.get("target").toString())
+
+            val donasi: Double = data.get("donated_cash").toString().toDouble()
+            val tDonasi: Double = data.get("target").toString().toDouble()
+            val format: NumberFormat = DecimalFormat("#,###")
+            val finalDonasi: String = "Rp. " + format.format(donasi).toString()
+            val targetDonasi: String = " terkumpul dari Rp. " + format.format(tDonasi).toString()
+
+            donasi_sekarang.setText(finalDonasi)
+            target_donasi.setText(targetDonasi)
+
             val a = data.get("donated_cash").toString().toFloat()
             val b = data.get("target").toString().toFloat()
-            val c = ((25000000/b) * 100).toInt()
+            val c = ((a/b) * 100).toInt()
+
             progress_galang.setProgress(c)
             Log.d("Value of c: ", c.toString())
-            jumlah_donasi.setText(data.get("donated_num").toString())
-            batas_galang.setText("Berakhir pada tanggal " + data.get("limit").toString())
+            jumlah_donasi.setText(data.get("donated_num").toString() + " donasi")
+            batas_galang.setText("Berakhir pada: " + data.get("limit").toString())
             description_title.setText(data.get("description_title").toString())
             description_date.setText(data.get("date").toString())
             description.setText(data.get("description").toString())
